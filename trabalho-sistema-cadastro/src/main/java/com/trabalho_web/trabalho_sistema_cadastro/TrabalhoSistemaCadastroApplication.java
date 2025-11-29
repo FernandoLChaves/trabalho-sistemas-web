542d9195052b4e922a920b989e80ffe59e02e9d1
Fernando Chaves <fernando_lc_silva@estudante.sesisenai.org.br>
2025-11-29 16:32:38 -0300

    Add files via upload

---
+++ trabalho-sistema-cadastro/src/main/java/com/trabalho_web/trabalho_sistema_cadastro/TrabalhoSistemaCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho_web.trabalho_sistema_cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoSistemaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoSistemaCadastroApplication.class, args);
+	}
+
+}
---
+++ trabalho-sistema-cadastro/src/test/java/com/trabalho_web/trabalho_sistema_cadastro/TrabalhoSistemaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho_web.trabalho_sistema_cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoSistemaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
